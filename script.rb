73ae8764cb40041833ee3be380a6733e2d68d123
Caleb Smith <karagenit@outlook.com>
2017-10-11 00:40:22 +0000

    Ditch CSV Writer, Use String

--- script.rb
+++ script.rb
@@ -1,12 +1,21 @@
 #!/usr/bin/env ruby
 
-input = CSV.read('input.csv')
+require 'csv'
+
+input = CSV.read('input.csv')[0]
 
 print 'Enter Elements per Row: '
 elems = gets.chomp.to_i
 
-CSV.open('output.csv', 'wb') do |out|
-  until input == []
-    out << input.pop elems
+string = ''
+
+until input == []
+  row = input.pop(elems)
+  string = string + row.pop.to_i.to_s
+  until row == []
+    string += ', ' + row.pop.to_i.to_s
   end
+  string = string + "\n"
 end
+
+IO.write('output.csv', string);
